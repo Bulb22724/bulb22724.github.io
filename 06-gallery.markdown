0a46998ad2f4f6cd5db968b6c4ef65f565d4431e
electrovoyage <75689188+electrovoyage@users.noreply.github.com>
2026-04-04 21:44:29 +0300

    избранные фото сделаны очередной категорией, addEventListener вместо onload, gallery.js вынесен в функцию для избежания конфликтов переменных (type="module" изолировал скрипт от его элемента-родителя)

--- 06-gallery.markdown
+++ 06-gallery.markdown
@@ -4,18 +4,9 @@ title: Галерея
 permalink: /gallery
 ---
 
-{% include heading.html text="Избранные фото" id="favorites" level=1%}
-{% for photo in site.data.gallery.favorites %}
-![{{photo.label}}]({{photo.url}})
-<p style="text-align: center">{{photo.label}}</p>
-{% unless forloop.last %}
----
-{% endunless %}
-{% endfor %}
-
 ---
 <!--script src="/assets/scripts/gallery.js"-->
-{% for category in site.data.gallery.others %}
+{% for category in site.data.gallery.categories %}
 {% include heading.html text=category.name id=category.id level=1 %}
-{% include gallery-embed.html photos=category.photos %}
+{% include gallery-embed.html photos=category.photos id=category.id %}
 {% endfor %}
--- _includes/gallery.js
+++ _includes/gallery.js
@@ -1,34 +1,37 @@
-let photos = [
-    {% for photo in include.photos  %}
-        {label: "{{photo.label | escape}}", url: "{{category.folder}}/{{photo.url}}"},
-    {% endfor %}
-]
-
-let maxindex = photos.length - 1
-let minindex = 0
-
-let index = 0
-let parent = document.currentScript.parentElement
-let image = parent.querySelector('.gallery-image')
-let label = parent.querySelector('.gallery-desc')
-
-let nextButton = parent.querySelector('.next')
-let backButton = parent.querySelector('.back')
-
-nextButton.addEventListener('click', function() {
-    index = (index == maxindex ? minindex : index + 1)
-    display()
-})
-
-backButton.addEventListener('click', function() {
-    index = (index == minindex ? maxindex : index - 1)
-    display()
-})
-
-function display() {
-    let photo = photos[index]
-    image.setAttribute('src', photo.url)
-    label.innerHTML = photo.label
-}
-
-window.onload = display
+(function() {
+    let photos = [
+        {% for photo in include.photos  %}
+            {label: "{{photo.label | escape}}", url: "{{category.folder}}/{{photo.url}}"},
+        {% endfor %}
+    ]
+    
+    let maxindex = photos.length - 1
+    let minindex = 0
+    
+    let index = 0
+    let parent = document.currentScript.parentElement
+    let image = parent.querySelector('.gallery-image')
+    let label = parent.querySelector('.gallery-desc')
+    
+    let nextButton = parent.querySelector('.next')
+    let backButton = parent.querySelector('.back')
+    
+    nextButton.addEventListener('click', function() {
+        index = (index == maxindex ? minindex : index + 1)
+        display()
+    })
+    
+    backButton.addEventListener('click', function() {
+        index = (index == minindex ? maxindex : index - 1)
+        display()
+    })
+    
+    function display() {
+        let photo = photos[index]
+        image.setAttribute('src', photo.url)
+        label.innerHTML = photo.label
+    }
+    
+    //window.onload = display
+    window.addEventListener('load', display)
+})()
