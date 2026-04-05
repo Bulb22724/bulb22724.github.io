45aafd999c933eddc3bc005fbc1c926eead28c0d
electrovoyage <75689188+electrovoyage@users.noreply.github.com>
2026-04-05 12:23:28 +0300

    если в галерее одно изображение, не показывать стрелки

--- _includes/gallery.js
+++ _includes/gallery.js
@@ -4,34 +4,39 @@
             {label: "{{photo.label | escape}}", url: "{{category.folder}}/{{photo.url}}"},
         {% endfor %}
     ]
-    
+
     let maxindex = photos.length - 1
     let minindex = 0
-    
+
     let index = 0
     let parent = document.currentScript.parentElement
     let image = parent.querySelector('.gallery-image')
     let label = parent.querySelector('.gallery-desc')
-    
+
     let nextButton = parent.querySelector('.next')
     let backButton = parent.querySelector('.back')
-    
+
+    if (maxindex == 0) {
+        nextButton.style.display = "none"
+        backButton.style.display = "none"
+    }    
+
     nextButton.addEventListener('click', function() {
         index = (index == maxindex ? minindex : index + 1)
         display()
     })
-    
+
     backButton.addEventListener('click', function() {
         index = (index == minindex ? maxindex : index - 1)
         display()
     })
-    
+
     function display() {
         let photo = photos[index]
         image.setAttribute('src', photo.url)
         label.innerHTML = photo.label
     }
-    
+
     //window.onload = display
     window.addEventListener('load', display)
 })()
